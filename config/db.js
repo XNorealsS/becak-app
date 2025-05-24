15d04f1a78eabd91918054cdf74b45cc3968f5fa
XNOREALSZ <muhammadrifaa973@gmail.com>
2025-05-24 11:45:15 +0700

    x

--- config/db.js
+++ config/db.js
@@ -3,9 +3,10 @@ require('dotenv').config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
 db.connect((err) => {
